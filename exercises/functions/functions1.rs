c8f389562bcb5d256d64b250cd9bcbf1e74aa6d0
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2025-01-16 21:15:44 +0800

    update

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -7,6 +7,20 @@ fn main() {
     call_me();
 }
 
+/// This function is responsible for printing a message to the console.
+///
+/// # Purpose
+///
+/// The `call_me` function is designed to simulate a call to a specific number.
+/// It prints a predefined message to the console.
+///
+/// # Parameters
+///
+/// This function does not take any parameters.
+///
+/// # Return Value
+///
+/// This function does not return any value. It only prints a message to the console.
 fn call_me() {
     println!("Ring! Call number 1!");
 }
